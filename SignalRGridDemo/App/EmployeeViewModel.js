e31061506c4dfb929e800bdd22d9274bd9a5cc3d
zameer <ahamedsmr@yahoo.com>
2013-07-09 14:29:11 +0530

    Update EmployeeViewModel.js
    Included property observable

--- SignalRGridDemo/App/EmployeeViewModel.js
+++ SignalRGridDemo/App/EmployeeViewModel.js
@@ -1,14 +1,46 @@
-﻿var EmployeeViewModel = function () {
+var EmployeeViewModel = function () {
     var self = this;
     self.employees = ko.observableArray();
-    self.loading = ko.observable(false);
+    self.loading = ko.observable(true);
 
-    $.getJSON('api/Employees', function (data) {
-        self.employees(data);
+    self.watchModel = function (model, callback) {
+        for (var key in model) {
+            if (model.hasOwnProperty(key) && ko.isObservable(model[key])) {
+                self.subscribeToProperty(model, key, function (key, val) {
+                    callback(model, key, val);
+                });
+            }
+        }
+    }
+
+    self.subscribeToProperty = function (model, key, callback) {
+        model[key].subscribe(function (val) {
+            callback(key, val);
+        });
+    }
+
+    self.modelChanged = function (model, key, val) {
+        alert(model.Id);
+        alert(key);
+        alert(val);
+    }
+
+    $.getJSON("api/Employees", function (data) {
+        //self.employees(data);
+        self.employees(ko.utils.arrayMap(data, function (employee) {
+            var objEmployee = {
+                Id: employee.Id,
+                Name: ko.observable(employee.Name),
+                Email: ko.observable(employee.Email),
+                Salary: ko.observable(employee.Salary)
+            }
+            self.watchModel(objEmployee, self.modelChanged);
+            return objEmployee;
+        }));
         self.loading(false);
     });
 }
+
 $(function () {
     ko.applyBindings(new EmployeeViewModel());
 });
-
