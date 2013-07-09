c24502ac9a97ad14936adf68ac663abe92eefa16
zameer <ahamedsmr@yahoo.com>
2013-07-09 14:28:15 +0530

    Create CustomBindings.js

---
+++ SignalRGridDemo/App/CustomBindings.js
@@ -0,0 +1,8 @@
+ko.bindingHandlers.displayMoney = {
+    init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+        $(element).html('$' + valueAccessor()().toFixed(2));
+    },
+    update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+        $(element).html('$' + valueAccessor()().toFixed(2));
+    }
+};
